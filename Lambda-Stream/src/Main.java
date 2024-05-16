10fa10072810c527f7ae0e0364ac834349e9baf3
Anton P. <made1of1fire@gmail.com>
2024-05-16 19:52:31 +0500

    feat(Test): added distincttest()

--- Lambda-Stream/src/Main.java
+++ Lambda-Stream/src/Main.java
@@ -2,48 +2,36 @@ import Entity.Learner;
 import Entity.Schoolar;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
+    private static List<Learner> learnerList = learnerListGenerator();
+
     public static void main(String[] args) {
-        List<Learner> learnerList = learnerListGenerator();
         System.out.println("Original List: ");
         learnerList.forEach(System.out::println);
         System.out.println("----------------");
 
-        System.out.println("Filtered List (avg >= 4.3): ");
-        learnerList.stream()
-                .map(learner -> (Schoolar) learner)
-                .filter(schoolar -> schoolar.getAvgGrade() >= 4.3)
-                .forEach(System.out::println);
-        System.out.println("----------------");
+        test1();
+        test2();
+        test3();
+        test4();
+        test5();
+        distinctTest();
+    }
 
-        System.out.println("Filtered List (Learners from Moscow in another list): ");
-        List<Learner> learnersFromMoscow = learnerList.stream()
-                .filter(l -> l.getCity() == "Moscow")
-                .toList();
-        learnersFromMoscow.forEach(System.out::println);
-        System.out.println("----------------");
+    private static void distinctTest() {
+        int[] ints = {1, 2, 5, 4, 3, 5, 5, 3, 2, 1};
+        Stream<Integer> integerStream = Arrays.stream(ints).boxed();
+        System.out.println("Distinct test\nOriginal Stream: " + Arrays.toString(ints));
+        System.out.println("After distinct: " + integerStream.distinct().toList());
+        integerStream.close();
+    }
 
-        System.out.println("Filtered and Sorted by reversed age (avg < 4.5 && class < 10)");
-        learnerList.stream()
-                .map(l -> (Schoolar) l)
-                .filter(s -> s.getAvgGrade() < 4.5 && s.getClassNumber() < 10)
-                .sorted((s1, s2) -> s2.getAge() - s1.getAge())
-                .forEach(System.out::println);
-        System.out.println("----------------");
-
-        System.out.println("AVG Age: ");
-        learnerList.stream()
-                .map(Learner::getAge)
-                .reduce(Integer::sum)
-                .ifPresent(reducedInt -> {
-                    System.out.println((double) reducedInt / learnerList.size());
-                });
-        System.out.println("----------------");
-
+    private static void test5() {
         System.out.println("Replaced all 'SPB' -> 'Saint Petersburg': ");
         learnerList.stream()
                 .forEach(learner -> {
@@ -52,6 +40,45 @@ public class Main {
                     }
                     System.out.println(learner);
                 });
+    }
+
+    private static void test4() {
+        System.out.println("AVG Age: ");
+        learnerList.stream()
+                .map(Learner::getAge)
+                .reduce(Integer::sum)
+                .ifPresent(reducedInt -> {
+                    System.out.println((double) reducedInt / learnerList.size());
+                });
+        System.out.println("----------------");
+    }
+
+    private static void test3() {
+        System.out.println("Filtered and Sorted by reversed age (avg < 4.5 && class < 10)");
+        learnerList.stream()
+                .map(l -> (Schoolar) l)
+                .filter(s -> s.getAvgGrade() < 4.5 && s.getClassNumber() < 10)
+                .sorted((s1, s2) -> s2.getAge() - s1.getAge())
+                .forEach(System.out::println);
+        System.out.println("----------------");
+    }
+
+    private static void test2() {
+        System.out.println("Filtered List (Learners from Moscow in another list): ");
+        List<Learner> learnersFromMoscow = learnerList.stream()
+                .filter(l -> l.getCity() == "Moscow")
+                .toList();
+        learnersFromMoscow.forEach(System.out::println);
+        System.out.println("----------------");
+    }
+
+    private static void test1() {
+        System.out.println("Filtered List (avg >= 4.3): ");
+        learnerList.stream()
+                .map(learner -> (Schoolar) learner)
+                .filter(schoolar -> schoolar.getAvgGrade() >= 4.3)
+                .forEach(System.out::println);
+        System.out.println("----------------");
     }
 
     public static List<Learner> learnerListGenerator() {
