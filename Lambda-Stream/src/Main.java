74a58fe664405383037234eac2a7c79b840688a3
Anton P. <made1of1fire@gmail.com>
2024-05-16 13:43:21 +0500

    feat(Test): added 5 tests (with Lambda Expressions and Streams)

--- Lambda-Stream/src/Main.java
+++ Lambda-Stream/src/Main.java
@@ -2,12 +2,56 @@ import Entity.Learner;
 import Entity.Schoolar;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Learner> learnerList = learnerListGenerator();
-        System.out.println(learnerList);
+        System.out.println("Original List: ");
+        learnerList.forEach(System.out::println);
+        System.out.println("----------------");
+
+        System.out.println("Filtered List (avg >= 4.3): ");
+        learnerList.stream()
+                .map(learner -> (Schoolar) learner)
+                .filter(schoolar -> schoolar.getAvgGrade() >= 4.3)
+                .forEach(System.out::println);
+        System.out.println("----------------");
+
+        System.out.println("Filtered List (Learners from Moscow in another list): ");
+        List<Learner> learnersFromMoscow = learnerList.stream()
+                .filter(l -> l.getCity() == "Moscow")
+                .toList();
+        learnersFromMoscow.forEach(System.out::println);
+        System.out.println("----------------");
+
+        System.out.println("Filtered and Sorted by reversed age (avg < 4.5 && class < 10)");
+        learnerList.stream()
+                .map(l -> (Schoolar) l)
+                .filter(s -> s.getAvgGrade() < 4.5 && s.getClassNumber() < 10)
+                .sorted((s1, s2) -> s2.getAge() - s1.getAge())
+                .forEach(System.out::println);
+        System.out.println("----------------");
+
+        System.out.println("AVG Age: ");
+        learnerList.stream()
+                .map(Learner::getAge)
+                .reduce(Integer::sum)
+                .ifPresent(reducedInt -> {
+                    System.out.println((double) reducedInt / learnerList.size());
+                });
+        System.out.println("----------------");
+
+        System.out.println("Replaced all 'SPB' -> 'Saint Petersburg': ");
+        learnerList.stream()
+                .forEach(learner -> {
+                    if (learner.getCity() == "SPB") {
+                        learner.setCity("Saint-Petersburg");
+                    }
+                    System.out.println(learner);
+                });
     }
 
     public static List<Learner> learnerListGenerator() {
