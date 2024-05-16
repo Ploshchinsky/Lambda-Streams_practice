6920bcc5b4dac51e616478d547aa62fc7d2092bf
Anton P. <made1of1fire@gmail.com>
2024-05-16 12:55:18 +0500

    feat(Entity): added general absctract class Learner

---
+++ Lambda-Stream/src/Entity/Learner.java
@@ -0,0 +1,33 @@
+package Entity;
+
+import java.util.Objects;
+
+public abstract class Learner implements Comparable<Learner> {
+    private String name;
+    private int age;
+    private String city;
+
+    public abstract String getName();
+
+    public abstract void setName(String name);
+
+    public abstract int getAge();
+
+    public abstract void setAge(int age);
+
+    public abstract String getCity();
+
+    public abstract void setCity(String city);
+
+    @Override
+    public abstract boolean equals(Object o);
+
+    @Override
+    public abstract int hashCode();
+
+    @Override
+    public abstract String toString();
+
+    @Override
+    public abstract int compareTo(Learner o);
+}
